92a6d0f07611796b70ef080984179aa7ce8ef439
Aleksandar Jovanovic <aleksandar04081@gmail.com>
2022-01-13 07:25:21 -0800

    Add files via upload

---
+++ Bank App 4.0/entryPage/submitForm.js
@@ -0,0 +1,13 @@
+function submitForm() {
+    let newUser = {
+      firstName: inputFirstName.value,
+      lastName: inputLastName.value,
+      emailName:inputEmail.value,
+      userName: inputUserName.value,
+      password: inputPassword.value,
+    };
+  
+    localStorage.setItem("newUser", JSON.stringify(newUser));
+    resetForm();
+    displayLogInPage();
+  }
